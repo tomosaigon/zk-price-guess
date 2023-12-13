e8be365a953cad980ae59d95e2380632ce4cd69a
Tomo <tomo@crowdeo.com>
2023-12-13 16:31:01 +0700

    feat: commitGuessesRoot, use updateBest - modifier guessingNotAllowed

--- packages/hardhat/contracts/Guess.sol
+++ packages/hardhat/contracts/Guess.sol
@@ -2,18 +2,18 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 struct G1Point {
-  uint X;
-  uint Y;
+	uint X;
+	uint Y;
 }
 // Encoding of field elements is: X[0] * z + X[1]
 struct G2Point {
-  uint[2] X;
-  uint[2] Y;
+	uint[2] X;
+	uint[2] Y;
 }
 struct Proof {
-  G1Point a;
-  G2Point b;
-  G1Point c;
+	G1Point a;
+	G2Point b;
+	G1Point c;
 }
 
 contract Guess {
@@ -26,6 +26,7 @@ contract Guess {
 	bool public btcPriceSet;
 
 	uint[] public hashedGuesses;
+	uint public guessesRoot;
 
 	bool public guessingIsAllowed = true;
 
@@ -38,6 +39,10 @@ contract Guess {
 
 	modifier guessingAllowed() {
 		require(guessingIsAllowed, "Guessing is not allowed");
+		_;
+	}
+	modifier guessingNotAllowed() {
+		require(!guessingIsAllowed, "Guessing is still allowed");
 		_;
 	}
 
@@ -53,10 +58,10 @@ contract Guess {
 	}
 
 	function hashedGuessesLength() public view returns (uint) {
-    return hashedGuesses.length;
-  }
-  
-	function closeGuessingAndResolvePrice() public {
+		return hashedGuesses.length;
+	}
+
+	function closeGuessingAndResolvePrice() public guessingAllowed {
 		guessingIsAllowed = false;
 
 		IPriceContract(priceContract).setBtcPrice();
@@ -65,42 +70,72 @@ contract Guess {
 		btcPriceSet = true;
 	}
 
-	function commitGuess(uint hashedGuess1, uint hashedGuess2) public payable guessingAllowed {
+	function commitGuess(
+		uint hashedGuess1,
+		uint hashedGuess2
+	) public payable guessingAllowed {
 		// require(msg.value >= ticketPrice, "Insufficient funds");
 
-    uint hashedGuess = uint(uint128(hashedGuess1) << 128 | uint128(hashedGuess2));
+		uint hashedGuess = uint(
+			(uint128(hashedGuess1) << 128) | uint128(hashedGuess2)
+		);
 		hashedGuesses.push(hashedGuess);
 
 		lastGuessBySender[msg.sender] = hashedGuesses.length - 1;
 	}
 
-  // main(
-  //   public field guess, 
-  //   private field nullifier,
-  //   private field[2] myHashedGuessTEST, // make test consistent
-  //   public field[2] nullifierHash,
-  //   public field[2] committedRoot, 
-  //   private field[8][2] hashedGuesses)
+	function commitGuessesRoot(uint root) public guessingNotAllowed {
+		// TODO calc from hashedGuesses
+		guessesRoot = root;
+	}
+
+	// main(
+	//   public field guess,
+	//   private field nullifier,
+	//   private field[2] myHashedGuessTEST, // make test consistent
+	//   public field[2] nullifierHash,
+	//   public field[2] committedRoot,
+	//   private field[8][2] hashedGuesses)
 	function updateBest(
-    Proof memory proof,
+		Proof memory proof,
 		uint guessValue,
-    uint nullifierHash1,
-    uint nullifierHash2
-	) public {
-		require(!guessingIsAllowed, "Guessing is allowed");
+		uint nullifierHash1,
+		uint nullifierHash2,
+		uint hashedGuessesRoot1,
+		uint hashedGuessesRoot2
+	) public guessingNotAllowed {
 		if (!btcPriceSet) {
 			closeGuessingAndResolvePrice();
 		}
 
 		// Verify the proof using the VerifierContract's verifyTX function
 		require(
-			IVerifierContract(verifierContract).verifyTX(proof, [guessValue, nullifierHash1, nullifierHash2, nullifierHash1, nullifierHash2]),
+			IVerifierContract(verifierContract).verifyTX(
+				proof,
+				[
+					guessValue,
+					nullifierHash1,
+					nullifierHash2,
+					hashedGuessesRoot1,
+					hashedGuessesRoot2
+				]
+			),
 			"Proof verification failed"
 		);
 
-    // Calculate nullifier
-    uint nullifier = uint(uint128(nullifierHash1) << 128 | uint128(nullifierHash2));
-		// Verify that nullifier is not already used
+		// Proof root matches commitment
+    // TODO zokrates vs solidity
+		// require(
+		// 	uint(
+		// 		(uint128(hashedGuessesRoot1) << 128) |
+		// 			uint128(hashedGuessesRoot2)
+		// 	) == guessesRoot,
+		// 	"Roots do not match"
+		// );
+
+		uint nullifier = uint(
+			(uint128(nullifierHash1) << 128) | uint128(nullifierHash2)
+		);
 		require(!nullifiers[nullifier], "Nullifier already used");
 		nullifiers[nullifier] = true;
 
@@ -124,7 +159,10 @@ contract Guess {
 }
 
 interface IVerifierContract {
-  function verifyTX(Proof memory proof, uint[5] memory inputs) external returns (bool);
+	function verifyTX(
+		Proof memory proof,
+		uint[5] memory inputs
+	) external returns (bool);
 }
 
 interface IPriceContract {
