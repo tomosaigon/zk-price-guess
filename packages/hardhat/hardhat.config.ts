e8bf5d448fd7cdc473590ac17600391ea3446896
Tomo <tomo@crowdeo.com>
2023-12-14 01:19:36 +0700

    feat: sepolia

--- packages/hardhat/hardhat.config.ts
+++ packages/hardhat/hardhat.config.ts
@@ -47,7 +47,7 @@ const config: HardhatUserConfig = {
       accounts: [deployerPrivateKey],
     },
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
+      url: process.env.RPC_ENDPOINT_URL ? process.env.RPC_ENDPOINT_URL : `https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`,
       accounts: [deployerPrivateKey],
     },
     goerli: {
